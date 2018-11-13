8a3b37af5e65b7884634c895ed44575a1d3c6bc1
bryan <bryan_hallman29@yahoo.com>
2018-11-13 00:14:48 -0500

    updated for Lab 23

--- src/main/java/com/example/Lab21CoffeeShopUserRegistration/Coffeecontroller.java
+++ src/main/java/com/example/Lab21CoffeeShopUserRegistration/Coffeecontroller.java
@@ -1,17 +1,27 @@
 package com.example.Lab21CoffeeShopUserRegistration;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
 @Controller
 public class Coffeecontroller {
+	
+	@Autowired
+	//private MenuItemDao menuItemDao;
+	private MenuService menuService;
 
 	@RequestMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index");
 	}
+	
 	
 	@RequestMapping("/userregistration")
 	public ModelAndView showUserregistration()
@@ -19,15 +29,18 @@ public class Coffeecontroller {
 			return new ModelAndView("userregistration");
 	}
 	
+	@RequestMapping("/menuservice")
+	public ModelAndView showMenuService() {
+		ModelAndView mv=new ModelAndView("menuservice");
+		return mv;
+	}
+	
 	@RequestMapping("/hello")
 	public ModelAndView showHello(@RequestParam("first")String first,
 			@RequestParam("last") String last,
 			@RequestParam("phonenumber") String phonenumber,
 			@RequestParam("email")String email,
-			@RequestParam("favorite")String favorite)
-			
-	
-	 {
+			@RequestParam("favorite")String favorite) {
 	ModelAndView mv=new ModelAndView("hello");
 	mv.addObject("first", first);
 	mv.addObject("last", last);
@@ -36,6 +49,14 @@ public class Coffeecontroller {
 	mv.addObject("favorite", favorite);
 	
 	return mv;
+	}
+	
+	@RequestMapping("/MenuService")
+	public ModelAndView showMenuService (@RequestParam (name="category", required = false) String category ) {
+		
+		ModelAndView mav = new ModelAndView("menuservice");
+		mav.addObject("MenuItem",menuService.getMenu() );
+		return mav;
 	}
 	
 	
---
+++ src/main/java/com/example/Lab21CoffeeShopUserRegistration/MenuItem.java
@@ -0,0 +1,58 @@
+package com.example.Lab21CoffeeShopUserRegistration;
+
+public class MenuItem {
+Long id;
+String name;
+String description;
+Double price;
+
+public MenuItem() {
+	
+}
+
+public MenuItem(Long id, String name, String description, Double price) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.description = description;
+	this.price = price;
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public Double getPrice() {
+	return price;
+}
+
+public void setPrice(Double price) {
+	this.price = price;
+}
+
+@Override
+public String toString() {
+	return "MenuItem [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
+}
+
+
+
+
+
+
+
+
+	
+}
